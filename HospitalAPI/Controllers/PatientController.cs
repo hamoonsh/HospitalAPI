307ff572287f9cc4171fc8ffed2ef5b5d04f257a
Hamoon SH <hm@gm.com>
2020-07-06 10:55:48 +0430

    api name changed

---
+++ HospitalAPI/Controllers/PatientController.cs
@@ -0,0 +1,41 @@
+﻿using HospitalAPI.Models.Entities;
+using HospitalAPI.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HospitalAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+
+
+
+        private readonly IPatientRepository _patientRepository;
+        private readonly GeneralMethods _generalMethods;
+
+        public PatientController(IPatientRepository patientRepository, GeneralMethods generalMethods)
+        {
+
+            _patientRepository = patientRepository;
+            _generalMethods = generalMethods;
+        }
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> RegPatient([FromBody] Patient patient)
+        {
+            // TODO
+            // if (patient.MobileNumber.IsVerified)
+            // {
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            if (await _generalMethods.AddAsyncOrUpdate(patient) && await _generalMethods.SaveChangesAsync())
+                return Ok();
+            return StatusCode(500);
+            // }
+        }
+    }
+}
